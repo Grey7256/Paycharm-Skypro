afd9e243693a9fc5361826cf13ed8afd14b846d6
Jakeg <jakewell72@gmail.com>
2024-09-30 14:41:30 +0300

    add changes at processing.py

--- src/processing.py
+++ src/processing.py
@@ -8,26 +8,23 @@ list_of_dicts = [
 ]
 
 
-def filter_by_state(list_of_dict: list[dict[str, Any]], state: str = "EXECUTED") -> Any:
+def filter_by_state(list_of_dicts: list[dict[str, Any]], state: str = "EXECUTED") -> list[dict[str, Any]]:
     """Функция принимает список словарей и значение для ключа и возвращает новый
     список содержащий только те словари у которых ключ содержит переданное значение"""
-
-    return [d for d in list_of_dict if d.get("state") == state]
+    new_list = []
+    for i in list_of_dicts:
+        if i.get("state") == state:
+            new_list.append(i)
+    return new_list
 
 
 print(filter_by_state(list_of_dicts))
 
 
-def sort_by_date(
-    list_of_dict: list[dict[str, Any]], reverse: bool = True
-) -> list[dict[str, Any]]:
+def sort_by_date(list_of_dicts: list[dict[str, Any]], reverse_list: bool = True) -> list[dict[str, Any]]:
     """Функция принимает список и сортирует его по убыванию"""
 
-    sorted_list = sorted(
-        list_of_dict,
-        key=lambda new_list_of_dict: new_list_of_dict["date"],
-        reverse=reverse,
-    )
+    sorted_list = sorted(list_of_dicts, key=lambda x: x["date"], reverse=reverse_list)
 
     return sorted_list
 
